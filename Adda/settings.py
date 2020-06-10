88503452811fe433559ea357293b02f90a0bdc73
badalkumar <badal8750316909@gmail.com>
2020-06-10 13:22:24 +0530

    SET debug=false

--- Adda/settings.py
+++ Adda/settings.py
@@ -150,6 +150,8 @@ if os.getcwd() == '/app':
     
     ALLOWED_HOSTS = ['*']
     
+    DEBUG = False
+    
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
     STATICFILES_DIRS = (
