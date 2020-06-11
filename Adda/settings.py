4d30aacf41ab7c004f23a33499fb9f6dc9e06879
badalkumar <badal8750316909@gmail.com>
2020-06-11 15:14:14 +0530

    v2

--- Adda/settings.py
+++ Adda/settings.py
@@ -152,8 +152,6 @@ if os.getcwd() == '/app':
     }
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     
-    DEBUG=False
-    
     ALLOWED_HOSTS = ['*']
     
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
