7c71619e4af4167a4196c435d702d0217e58db72
badalkumar <badal8750316909@gmail.com>
2020-06-12 13:02:24 +0530

    v5

--- myblog/views.py
+++ myblog/views.py
@@ -30,3 +30,29 @@ def read(request, blog_title):
                'new_comment': new_comment,
                'comment_form': comment_form}
     return render(request, 'blog/read.html', context)
+
+# WITH AUTHORIZED USER
+# def read(request, blog_title):
+#     blog = Myblog.objects.get(title=blog_title)
+#     comments = blog.comments.filter(active=True)
+#     new_comment = None
+#     # Comment posted
+#     if request.method == 'POST':
+#         comment_form = CommentForm(data=request.POST)
+#         if comment_form.is_valid():
+#             if not request.user.is_authenticated:
+#                 return HttpResponse("Please do login")
+#             # Create Comment object but don't save to database yet
+#             new_comment = comment_form.save(commit=False)
+#             # Assign the current post to the comment
+#             new_comment.post = blog
+#             # Save the comment to the database
+#             new_comment.save()
+#     else:
+#         comment_form = CommentForm()
+
+#     context = {'blog': blog,
+#                'comments': comments,
+#                'new_comment': new_comment,
+#                'comment_form': comment_form}
+#     return render(request, 'blog/read.html', context)
