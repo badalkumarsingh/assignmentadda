37c7d0dd0d3aef43b4da07eb62ecfe99803cec96
badalkumar <badal8750316909@gmail.com>
2020-06-14 15:39:00 +0530

    ready

--- Adda/settings.py
+++ Adda/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rr+#hbec6w*+$+)((g469b0rz2w)j2l=*+jfvh6gqdp8w$z1p_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://assignmentadda.herokuapp.com/']
+ALLOWED_HOSTS = ['assignmentadda.herokuapp.com','127.0.0.1']
 
 
 # Application definition
