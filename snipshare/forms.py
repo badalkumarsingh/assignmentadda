5b4b1fd74ac0386e81c42f98eec9735045833c3d
Badal <badal8750316909@gmail.com>
2020-06-30 11:25:09 +0000

    deploy

--- snipshare/forms.py
+++ snipshare/forms.py
@@ -3,5 +3,5 @@ from .models import Snip
 class snipForm(forms.ModelForm):
     class Meta:
         model=Snip
-        fields=('text','title','lang','theme')
+        fields=('text','title','lang','theme','lines')
         labels={'title':'Snippet Title','text':'New Snippet','lang': 'Syntax Highlighting','theme': 'Choose Themes'}
